2ae8d53b0c408f68282eb0027ed3cfbc17be8e6d
Charles Ayeni <charles.work20@gmail.com>
2022-12-11 23:14:55 -0500

    Lots done need to fix images by using full search

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,6 +16,9 @@ using WeebLib.Novel.Parser;
 using WeebLib.Novel.Retrieval;
 using WeebLib.Novel;
 using WeebLib.Interfaces;
+using System.Net;
+using System.Security.Policy;
+using System.Xml.Linq;
 
 namespace AniTool
 {
@@ -36,6 +39,36 @@ namespace AniTool
         private void searchNovelBtn_MouseDown(object sender, MouseButtonEventArgs e)
         {
             searcher.Search(1, searchNovelBox.Text, NovelUtil.sourceToString(NovelUtil.NovelSources.FullNovel));
+            
+            WrapPanel mainStack = new();
+            mainStack.Orientation = Orientation.Horizontal;
+            novelScroll.Content = mainStack;
+            int number = 1;
+            searcher.getResults().ForEach((result) =>
+            {
+                string type = result.image.Split('.').Last();
+                DownloadImage(number, type, result.image);
+                
+                StackPanel panel = new();
+                panel.Orientation = Orientation.Vertical;
+                BitmapImage bitImage = new BitmapImage(new Uri(@$"C:\Users\charl\Documents\Programming\C#\WPF\AniTool\AniTool\resources\{number}.{type}"));
+                Image image = new Image() { Source = bitImage, Width = 1000, Height = 1300 };
+
+
+                panel.Children.Add(image);
+                //panel.Children.Add(new TextBlock() { Text = result.name });
+                //panel.Children.Add(new TextBlock() { Text = result.link });
+                mainStack.Children.Add(panel);
+                ++number;
+            });
+        }
+        
+        private static void DownloadImage(int number, string type, string url)
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadFile(new Uri(url), @$"C:\Users\charl\Documents\Programming\C#\WPF\AniTool\AniTool\resources\{number}.{type}");
+            }
         }
     }
 }
